079caf5758b10cf1f6dc064192822d47133f64d9
Mikel Martín <mikelmartin43@gmail.com>
2025-09-08 01:35:29 +0200

    feat: add character search bar

--- src/app/pages/characters/characters.component.ts
+++ src/app/pages/characters/characters.component.ts
@@ -7,10 +7,26 @@ import {Router} from '@angular/router';
 import {appRoutes} from '../../app.routes';
 import {characterRoutes} from './character.routes';
 import {CharacterItemComponent} from '../../shared/components/character-item/character-item.component';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import {CommonModule} from '@angular/common';
+import {FormControl, FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {debounceTime} from 'rxjs';
+
+const SEARCH_DEBOUNCE_TIME = 300;
 
 @Component({
   selector: 'app-characters',
-  imports: [CharacterItemComponent, MatButtonModule, MatIconModule],
+  imports: [
+    CharacterItemComponent,
+    CommonModule,
+    FormsModule,
+    MatButtonModule,
+    MatIconModule,
+    MatFormFieldModule,
+    MatInputModule,
+    ReactiveFormsModule,
+  ],
   templateUrl: './characters.component.html',
   styleUrl: './characters.component.scss',
 })
@@ -19,12 +35,28 @@ export class CharactersComponent implements OnInit {
 
   private router = inject(Router);
 
+  searchControl = new FormControl('');
+  search = '';
+
   characters: Character[] = [];
+
+  get filteredCharacters(): Character[] {
+    if (!this.search) return this.characters;
+    return this.characters.filter(character =>
+      character.name.toLowerCase().includes(this.search.toLowerCase())
+    );
+  }
 
   ngOnInit() {
     this.charactersService.all().subscribe({
       next: res => (this.characters = res),
     });
+
+    this.searchControl.valueChanges
+      .pipe(debounceTime(SEARCH_DEBOUNCE_TIME)) // 300ms de espera
+      .subscribe(value => {
+        this.search = value || '';
+      });
   }
 
   create() {
